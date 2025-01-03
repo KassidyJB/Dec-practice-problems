e7ddb2f55732a4cd855c52acc8db6b8fa6b3110b
KassidyJB <jeanbartkassidy51@gmail.com>
2025-01-03 11:43:48 +0000

    (P2) changes on choices 1, 2, 3, 4

--- P2.py
+++ P2.py
@@ -23,7 +23,7 @@ display_shape()
 
 ######Evaluating user input, setting the stage for customized responses based on choice number######
 def user_select():
-    choice = input(f"{name}, which shape do you want me to calculate the area of?\n")
+    choice = input(f"{name}, which shape do you want me to calculate the area of? (Enter number)\n")
     if choice == 1:
         choice_1()
     elif choice == 2:
@@ -38,35 +38,35 @@ user_select()
 
 ####Triangle function###
 def choice_1(): #Triangle area = (bh)/2 (ask for base and height)
-    tri_b = (str(float(input(f"\n Enter a base: \n>"))))
-    tri_h =  (str(float(input(f"\nEnter a height: \n>"))))
-    print(f"Triangle area: ")
+    tri_b = float(input(f"\n Enter a base: \n>"))
+    tri_h =  float(input(f"\nEnter a height: \n>"))
+    t_area = (tri_b*tri_h) / 2
 
-    
-    pass 
+choice_1()
 
 ####Square function####
 
 def choice_2(): #Square area = side length^2 (ask for side length)
-    sq =  (str(float(input(f"\nEnter a side length\n>"))))
-
-
-
-    pass 
+    sq =  float(input(f"\nEnter a side length\n>"))
+    s_area = sq*sq
+choice_2()
 
 ####Trapezoid Function#####
 
 def choice_3(): #Trapezoid area = (a+b/2)h (ask for bases and height)
-
+    t_a = float(input(f"\nEnter a number for base 1\n>"))
+    t_b = float(input(f"\nEnter a number for base 2\n>"))
+    
     pass 
 
 
 ####Circle Function#####
 
 def choice_4(): #Circle area = pi*r^2 (ask for radius)
-    pi = "3.14"
-    radii = input(f"\nEnter a radius\n>")
-    c_area = ({radii}^2)*pi
+    pi = 3.14 #assigning estimated pi value
+    radii = float(input(f"\nEnter a radius\n>"))#asking for user input for radius
+    #c_area = ({radii}**2)*pi#Attempt to calculate circle area but squaring operation is difficult
+    c_area = ({radii}*{radii})*pi#2nd attempt, trying to multiply radii by itself to square
 choice_4()
 
 
