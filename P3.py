ebe494fb8780fd1db010a647c28ca17126158759
KassidyJB <jeanbartkassidy51@gmail.com>
2025-01-03 11:46:41 +0000

    P3. comment commit

--- P3.py
+++ P3.py
@@ -1,3 +1,8 @@
 #Problem 3: Unique Characters in a String
 #DES:Create a function that determines whether all characters in a given string are unique.
 #  If the string has all unique characters, return True; otherwise, return False.
+
+##Thinking####
+# if the string is unique, but hello doesn't work, this could mean characters cannot be repeated.
+
+
