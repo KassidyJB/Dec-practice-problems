610af1c6c8b464e330dc1300fb97d42c7af2897e
KassidyJB <jeanbartkassidy51@gmail.com>
2025-01-03 10:55:41 +0000

    (P1) reverse, conditionals, var, input changes

--- P1.py
+++ P1.py
@@ -3,3 +3,14 @@
 # A palindrome is a word, phrase, number, or other sequence of characters that reads 
 # the same forwards and backwards (ignoring spaces, punctuation, and capitalization).
 
+##I will convert user input into a string, there's a possibility it will cause errors and overlook some edgecases 
+##like empty strings.
+word = str(input(f"\nEnter anything and we'll check if it is a palindrome \n>"))
+reverse = word [::-1] ##This is a slice statement that will start at the end of the word and 
+#subtract 1 to reverse each character in the word and return a reversed word. I will add the 'reverse' variable into 
+#the conditional.
+
+if word == " ":
+    print(f"\nThis is not a valid input. Please try Again!\n")
+else:
+    print(reverse)
